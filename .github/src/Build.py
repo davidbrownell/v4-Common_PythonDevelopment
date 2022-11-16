2e17654e53c301ca3a05f92a7d6e5852e5b9897b
David Brownell <db@DavidBrownell.com>
2022-11-16 09:35:02 -0800

    🎨 [refactor] Using modified CI functionality in Common_Foundation

--- .github/src/Build.py
+++ .github/src/Build.py
@@ -20,6 +20,8 @@ import sys
 
 from pathlib import Path
 
+import typer
+
 from Common_Foundation.ContextlibEx import ExitStack
 from Common_Foundation import PathEx
 from Common_Foundation import Types
@@ -30,11 +32,28 @@ github_src_root = PathEx.EnsureDir(foundation_root_dir / ".github" / "src")
 
 sys.path.insert(0, str(github_src_root))
 with ExitStack(lambda: sys.path.pop(0)):
-    from BuildImpl import CreateBuildInfoInstance  # type: ignore  # pylint: disable=import-error
+    from BuildImpl import CreateBuildInfoInstance, CreateTagsImpl  # type: ignore  # pylint: disable=import-error
 
 
 # ----------------------------------------------------------------------
 _build_info = CreateBuildInfoInstance(Path(__file__).parent)
+
+
+# ----------------------------------------------------------------------
+def CreateTags(
+    dry_run: bool=typer.Option(False, "--dry-run", help="Prints the command line used to create the tags and push them to GitHub, but does not invoke the functionality."),
+    yes: bool=typer.Option(False, "--yes", help="Answer yes to the 'are you sure' prompt."),
+    verbose: bool=typer.Option(False, "--verbose", help="Write verbose information to the terminal."),
+    debug: bool=typer.Option(False, "--debug", help="Write debug information to the terminal."),
+):
+    """Creates CI-related tags and pushes them to GitHub."""
+
+    return CreateTagsImpl(
+        dry_run=dry_run,
+        yes=yes,
+        verbose=verbose,
+        debug=debug,
+    )
 
 
 # ----------------------------------------------------------------------
